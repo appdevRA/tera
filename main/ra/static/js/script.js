5c4ed0226178b59223400cfff1bf2f1c1418639f
Ryan Talatagod <michaelbenedictlryan@gmail.com>
2021-09-13 13:21:22 +0800

    A
    A

--- main/ra/static/js/script.js
+++ main/ra/static/js/script.js
@@ -1,4 +1,6 @@
 // getting all required elements
+const ulTag = document.querySelector(".ulclass");
+let totalPages = 20;
 const searchWrapper = document.querySelector(".search");
 const inputBox = searchWrapper.querySelector("input");
 const suggBox = searchWrapper.querySelector(".autocom-box");
@@ -58,3 +60,63 @@ function showSuggestions(list){
     }
     suggBox.innerHTML = listData;
 }
+
+function element(totalPages, page) {
+    let liTag = '';
+    let activeLi;
+    let beforePages = page - 1;
+    let afterPages = page + 1;
+    if(page > 1) { //if page value is greater than 1 then add new li which is the previous button
+        liTag += `<li class="btn prev" onclick="element(totalPages, ${page - 1})"><span><i class="fas fa-angle-left"></i>Prev</span></li>`;
+    }
+    if (page > 2) { //if page value is greater than 2 then add new li tag with 1 value
+        liTag += `<li class="numb" onclick="element(totalPages, 1)"><span>1</span></li>`;
+        if (page > 3) { //if page value is greater than 3 then add new li tag with (...)
+            liTag += `<li class="dots"><span>...</span></li>`;
+        }
+    }
+
+    //how many pages or li show before the current li
+    if (page == totalPages) { //if page value is equal to totalPages then subtract by -2 to the beforePages value
+        beforePages = beforePages - 2;
+    }else if (page == totalPages - 1) { //else if page value is equal to totalPages by -1 then subtract by -1 to the beforePages value
+        beforePages = beforePages - 1;
+    }
+
+     //how many pages or li show after the current li
+    if (page == 1) { //if page value is equal to 1 then add by +2 to the afterPages value
+        afterPages = afterPages + 2;
+    }else if (page == 2) { //else if page value is equal to 2 then add by +1 to the afterPages value
+        afterPages = afterPages + 1;
+    }
+
+    for (let pageLength = beforePages; pageLength <= afterPages; pageLength++) {
+        if (pageLength > totalPages) {
+            continue;
+        }
+        if (pageLength == 0) { //if pageLength is equal to 0 then add +1 to the pagelength value
+            pageLength = pageLength + 1;
+        }
+        if (page == pageLength) { //if page value is equal to pageLength then assign the active string in the activeLi variable
+            activeLi = "active";
+        }else{ //else leave empty to the activeLi variable
+            activeLi = "";
+        }
+        liTag += `<li class="numb ${activeLi}" onclick="element(totalPages, ${pageLength})"><span>${pageLength}</span></li>`;
+    }
+
+    if (page < totalPages - 1) { //if page value is less than totalPages by -1 then show the last li or page which is 20
+        if (page < totalPages - 2) { //if page value is less than totalPages by -2 then show the last (...) before last page
+            liTag += `<li class="dots"><span>...</span></li>`;
+        }
+        liTag += `<li class="numb" onclick="element(totalPages, ${totalPages})"><span>${totalPages}</span></li>`;
+    }
+
+
+    if(page < totalPages) { //if page value is less than totalPages then add new li which is the next button
+        liTag += `<li class="btn next" onclick="element(totalPages, ${page + 1})"><span>Next<i class="fas fa-angle-right"></i></span></li>`;
+    }
+    ulTag.innerHTML = liTag;
+}
+element(totalPages, 5); //calling above function with passing values
+
