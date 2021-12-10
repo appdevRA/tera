5a8c30739099b4af7a4a9957c56a4bbce0704b31
appdevRA <mamondejar22@gmail.com>
2021-12-10 22:27:21 +0800

    Revert "Merge remote-tracking branch 'origin/jarry-for-admin' into mamon-backup-4"
    This reverts commit fd327978f473915b4a73547bdabddadb9f5070b5, reversing
    changes made to 7a890f5add0a20251509f4f6f50db2e7289ddc0e.

--- main/ra/static/js/datatables-simple-colleges.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('DOMContentLoaded', event => {
-    // Simple-DataTables
-    // https://github.com/fiduswriter/Simple-DataTables/wiki
-
-    const datatablesColleges = document.getElementById('datatablesColleges');
-    if (datatablesColleges) {
-        new simpleDatatables.DataTable(datatablesColleges);
-    }
-});
